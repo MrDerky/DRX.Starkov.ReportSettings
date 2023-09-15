6e5a243199ded87d612b083886a0524d6e9f0809
DirectumRXUser <directumrxuser@c.ru>
2023-09-15 14:20:06 +0500

    Изменение комментария при закрытии журнала

--- Starkov.ScheduledReports/Starkov.ScheduledReports.Server/ScheduleLog/ScheduleLogHandlers.cs
+++ Starkov.ScheduledReports/Starkov.ScheduledReports.Server/ScheduleLog/ScheduleLogHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -7,5 +7,15 @@ using Starkov.ScheduledReports.ScheduleLog;
 
 namespace Starkov.ScheduledReports
 {
+  partial class ScheduleLogServerHandlers
+  {
+
+    public override void BeforeSave(Sungero.Domain.BeforeSaveEventArgs e)
+    {
+      if (_obj.Status == Status.Closed)
+        _obj.Comment = string.Format(Starkov.ScheduledReports.ScheduleLogs.Resources.CancelByUser, Users.Current.Name);
+    }
+  }
+
 
 }
