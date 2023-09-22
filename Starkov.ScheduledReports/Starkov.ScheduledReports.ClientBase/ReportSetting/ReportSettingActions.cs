54ed9c05530d006525088049853996f6251fe95c
DirectumRXUser <directumrxuser@c.ru>
2023-09-22 21:01:14 +0500

    Переименование справочников

---
+++ Starkov.ScheduledReports/Starkov.ScheduledReports.ClientBase/ReportSetting/ReportSettingActions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Starkov.ScheduledReports.ReportSetting;
+
+namespace Starkov.ScheduledReports.Client
+{
+  partial class ReportSettingActions
+  {
+    public override void SetReport(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      Functions.SettingReport.SelectReport(_obj);
+    }
+
+    public override bool CanSetReport(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+      return base.CanSetReport(e);
+    }
+
+  }
+
+}
---
+++ Starkov.ScheduledReports/Starkov.ScheduledReports.Shared/ScheduleSetting/ScheduleSettingHandlers.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Starkov.ScheduledReports.ScheduleSetting;
+
+namespace Starkov.ScheduledReports
+{
+  partial class ScheduleSettingSharedHandlers
+  {
+
+    public virtual void PeriodNumberChanged(Sungero.Domain.Shared.IntegerPropertyChangedEventArgs e)
+    {
+      if (e.OldValue == e.NewValue )
+        return;
+      
+      var number = e.NewValue;
+      
+      if (e.NewValue < 1) //TODO вынести в настройку
+        number =_obj.PeriodNumber = 1;
+      else if (e.NewValue > 100)
+        number = _obj.PeriodNumber = 100;
+    }
+
+    public virtual void ReportSettingChanged(Starkov.ScheduledReports.Shared.ScheduleSettingReportSettingChangedEventArgs e)
+    {
+      if (Equals(e.OldValue, e.NewValue))
+        return;
+      
+      if (e.NewValue != null)
+      {
+        _obj.ModuleGuid = e.NewValue.ModuleGuid;
+        _obj.ModuleName = e.NewValue.ModuleName;
+        _obj.ReportGuid = e.NewValue.ReportGuid;
+        _obj.ReportName = e.NewValue.ReportName;
+      }
+      
+      if (string.IsNullOrEmpty(_obj.Name) || e.OldValue != null && e.OldValue.ReportName == _obj.Name)
+        Functions.SettingSchedule.FillName(_obj);
+      
+      PublicFunctions.SettingBase.SaveReportParams(_obj);
+    }
+
+  }
+}
